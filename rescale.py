347bad6dbfa15fe244ead874dbf92d145cb3a1e6
Craig A. Glastonbury <craig.glastonbury@kcl.ac.uk>
2016-12-26 14:23:36 +0000

    removed axis

--- rescale.py
+++ rescale.py
@@ -4,12 +4,9 @@ import scipy.misc
 import re,glob
 test_imgs = glob.glob("test_stg1/*.jpg")
 
+for filename in test_imgs:
+    image = ndimage.imread(filename, mode="RGB")
+    image_resized = misc.imresize(image, (480,640))
+    out_file=filename.split('.')[0].split('/')[1]
+    scipy.misc.toimage(image_resized).save('test_stg1/rescaled/%s_resized.jpg' %(out_file))
 
-images = []
-for root, dirnames, filenames in os.walk("test_stg1/"):
-    for filename in filenames:
-        if re.search("\.(jpg|jpeg|png|bmp|tiff)$", filename):
-            filepath = os.path.join(root, filename)
-            image = ndimage.imread(filepath, mode="RGB")
-            image_resized = misc.imresize(image, (480,640))
-            scipy.misc.toimage(image_resized).save('test_stg1/rescaled/%s_resized.jpg' %(filename))
