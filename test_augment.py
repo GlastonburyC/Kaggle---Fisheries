8ca2c83f68dc272e4d29182c65c4235b19ccc42f
Craig A. Glastonbury <craig.glastonbury@kcl.ac.uk>
2016-12-28 13:25:31 +0000

    Update test_augment.py

--- test_augment.py
+++ test_augment.py
@@ -10,8 +10,8 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 img_width = 299
 img_height = 299
 batch_size = 32
-nbr_test_samples = 2104
-nbr_augmentation = 10
+nbr_test_samples = 2104*5
+nbr_augmentation = 100
 
 FishNames = ['ALB_fish', 'BET_fish', 'DOL_fish', 'LAG_fish', 'NoF_fish', 'OTHER_fish', 'SHARK_fish', 'YFT_fish']
 
@@ -46,7 +46,7 @@ for idx in range(nbr_augmentation):
             classes = None,
             class_mode = None)
     test_image_list = test_generator.filenames
-    #print('image_list: {}'.format(test_image_list[:10]))
+        #print('image_list: {}'.format(test_image_list[:10]))
     print('Begin to predict for testing data ...')
     if idx == 0:
         predictions = InceptionV3_model.predict_generator(test_generator, nbr_test_samples)
@@ -60,8 +60,7 @@ f_submit = open(os.path.join(root_path, 'submit.csv'), 'w')
 f_submit.write('image,ALB,BET,DOL,LAG,NoF,OTHER,SHARK,YFT\n')
 for i, image_name in enumerate(test_image_list):
     pred = ['%.6f' % p for p in predictions[i, :]]
-    if i % 100 == 0:
-        print('{} / {}'.format(i, nbr_test_samples))
+    print('{} / {}'.format(i, nbr_test_samples))
     f_submit.write('%s,%s\n' % (os.path.basename(image_name), ','.join(pred)))
 
 f_submit.close()
