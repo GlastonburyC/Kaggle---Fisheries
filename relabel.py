84d83e9e45ee5bef71de325fe04ca75e874f96aa
Craig A. Glastonbury <craig.glastonbury@kcl.ac.uk>
2017-01-11 22:06:45 +0000

    Update relabel.py

--- relabel.py
+++ relabel.py
@@ -5,14 +5,16 @@ import os
 with open('relabel.txt', 'rb') as f:
     relabel = csv.reader(f)
     for row in relabel:
-        old_img_path = row[0]
-        new_img_path = row[1]+'/'+row[0].split('/')[1]
-        os.system('cp %s %s' % (old_img_path,new_img_path))
+        old_img_path = row[0].split('_r')[0].split('/')[0]+'_fish/'+row[0].split('_r')[0].split('/')[1]
+        new_img_path = row[1]+'_fish/'
+        os.system('cp %s_* %s' % (old_img_path,new_img_path))
 
 
 with open('relabel.txt', 'rb') as f:
     relabel = csv.reader(f)
     for row in relabel:
-        old_img_path = row[0]
-        os.system('rm %s' % (old_img_path))
+        old_img_path = row[0].split('_r')[0].split('/')[0]+'_fish/'+row[0].split('_r')[0].split('/')[1]
+        os.system('rm %s_*' % (old_img_path))
 
+
+os.system('mv ALB_fish/img_01452_box0.jpg LAG_fish/')
