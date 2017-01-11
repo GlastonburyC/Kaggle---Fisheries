28a5e9381ff98f6f4f7d5ac4d767b8065bf8e528
Craig A. Glastonbury <craig.glastonbury@kcl.ac.uk>
2017-01-11 01:18:22 +0000

    Update extract_test_bbox_scale.py

--- extract_test_bbox_scale.py
+++ extract_test_bbox_scale.py
@@ -10,15 +10,20 @@ import scipy.misc
 from scipy import ndimage, misc
 
 
-with open('30000_val_lstm_rezoom.json') as data_file:
+with open('30000_960x640_val_lstm_rezoom.json') as data_file:
 	all_json = json.load(data_file)
 
+
+
 for i,entry in enumerate(all_json):
-	image_path = '../test_stg1/rescaled/'+all_json[i]['image_path']
+	img_score_list=[]
+	image_path = '../test_stg1/rescaled_640_960/'+all_json[i]['image_path']
 	#species = all_json[i]['image_path'].split('/')[1]
-	img = cv2.imread(image_path,1)
+	img = cv2.imread(image_path,cv2.IMREAD_COLOR)
+	a = 0
 	for j in range(0,len(all_json[i]['rects'])):
 		score = all_json[i]['rects'][j]['score']
+		img_score_list.append(score)
 		y1 = int(round(all_json[i]['rects'][j]['y1']))
 		if y1 < 0:
 			y1 = 0
@@ -31,14 +36,41 @@ for i,entry in enumerate(all_json):
 		x2 = int(round(all_json[i]['rects'][j]['x2']))
 		if x2 < 0:
 			x2 = 0
-		if score > 0.5:
+		if score >= 0.7:
 			sub_img = img[y1:y2,x1:x2]
 			height, width, channels = sub_img.shape
-			if height < 30 and width < 30:
-				next
+			#if height < 30 and width < 30:
+			#	next
 				# removing errors when manually annotating thousands of fish...
-			else:
-				im_file = all_json[i]['image_path'].split('.')[0]
-				image_resized = misc.imresize(sub_img, (299,299))
-				scipy.misc.toimage(image_resized).save('../test_stg1/test_fishes/%s_box%s.jpg' %(im_file,str(j)))
-
+			#else:
+			im_file = all_json[i]['image_path'].split('.')[0]
+			image_resized = misc.imresize(sub_img, (299,299))
+			cv2.imwrite('../debug_img_quality/score%s_%s_box%s.jpg' %(score,im_file,str(j)),image_resized)
+			print str(score)+'\n'
+			a = 1
+	if (a == 0):
+		j = img_score_list.index(max(img_score_list))
+		y1 = int(round(all_json[i]['rects'][j]['y1']))
+		if y1 < 0:
+			y1 = 0
+		y2 = int(round(all_json[i]['rects'][j]['y2']))
+		if y2 < 0:
+			y2 = 0
+		x1 = int(round(all_json[i]['rects'][j]['x1']))
+		if x1 < 0:
+			x1 = 0
+		x2 = int(round(all_json[i]['rects'][j]['x2']))
+		if x2 < 0:
+			x2 = 0
+		#if score > 0.7:
+		score = all_json[i]['rects'][j]['score']
+		sub_img = img[y1:y2,x1:x2]
+		height, width, channels = sub_img.shape
+		if height < 30 and width < 30:
+			next
+				# removing errors when manually annotating thousands of fish...
+		else:
+			im_file = all_json[i]['image_path'].split('.')[0]
+			image_resized = misc.imresize(sub_img, (299,299))
+			cv2.imwrite('../debug_img_quality/score%s_%s_box%s.jpg' %(score,im_file,str(j)),image_resized)
+			print str(score)+'\n'
