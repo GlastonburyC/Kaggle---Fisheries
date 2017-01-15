3beba05f6cf9ebcf1d6fd5e3c98a8fcff876a868
Craig A. Glastonbury <craig.glastonbury@kcl.ac.uk>
2017-01-15 00:04:12 +0000

    k-fold split

--- val_train_split.py
+++ val_train_split.py
@@ -4,29 +4,28 @@ import json
 import itertools
 import random
 
-valp = 0.20
-trainp = 0.80
+rand_seeds=random.sample(range(1, 1000000), 5)
 
-labels = ['alb','bet','dol','lag','shark','other','yft']
+for i in xrange(0,4):
+	random.seed(rand_seeds[i])
+	valp = 0.20
+	trainp = 0.80
+	labels = ['alb','bet','dol','lag','shark','other','yft']
+	all_json=[]
+	for label in labels:
+		with open('%s.json' %(label)) as data_file:
+			data = json.load(data_file)
+			all_json.append(data)
+	all_json = list(itertools.chain(*all_json))
+	random.shuffle(all_json)
+	prop = int(round(len(all_json)*valp))
+	train_data = all_json[prop:]
+	test_data = all_json[:prop]
+	with open('train80_%s.json' % (i), 'w') as json_file:
+		json.dump(train_data,json_file, ensure_ascii=False,indent=4)
+	with open('validation20_%s.json' % (i), 'w') as json_file:
+		json.dump(test_data,json_file, ensure_ascii=False,indent=4)
 
-all_json=[]
-
-for label in labels:
-	with open('%s.json' %(label)) as data_file:
-		data = json.load(data_file)
-		all_json.append(data)
-
-all_json = list(itertools.chain(*all_json))
-
-random.shuffle(all_json)
-
-prop = int(round(len(all_json)*valp))
-
-train_data = all_json[prop:]
-test_data = all_json[:prop]
-
-with open('train80.json', 'w') as json_file:
-	json.dump(train_data,json_file, ensure_ascii=False,indent=4)
-
-with open('validation20.json', 'w') as json_file:
-	json.dump(test_data,json_file, ensure_ascii=False,indent=4)
+with open('val_train_split.seeds','w') as seed_file:
+	for item in rand_seeds:
+  		seed_file.write("%s\n" % item)
