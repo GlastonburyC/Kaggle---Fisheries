30e65e944c80d04d57d2e1c8a949ef415cef6a8b
Craig A. Glastonbury <craig.glastonbury@kcl.ac.uk>
2017-01-22 20:19:53 +0000

    Update rescale.py

--- rescale.py
+++ rescale.py
@@ -9,5 +9,5 @@ for filename in test_imgs:
     image = ndimage.imread(filename, mode="RGB")
     image_resized = misc.imresize(image, (480,640))
     out_file=filename.split('.')[0].split('/')[1]
-    scipy.misc.toimage(image_resized).save('test_stg1/rescaled/%s_resized.jpg' %(out_file))
+    cv2.imwrite('test_stg1/rescaled/%s_resized.jpg' %(out_file))
 
